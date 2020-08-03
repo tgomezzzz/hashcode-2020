07c1d36c0901b7a55878986dc6ea99dcb892b6d4
Tommy Gomez <tjk2132@columbia.edu>
2020-08-03 12:21:38 -0700

    read file passed in as command line argument

--- read-data.py
+++ read-data.py
@@ -1,2 +1,16 @@
+import sys
+
+def readData(file):
+    data = None
+    try:
+        data = open(file)
+    except: 
+        print("Error: file " + file + " could not be opened")
+    print("printed")
+
 if __name__ == "__main__":
-    print("hello world!")
+    if (len(sys.argv) != 2):
+        print("Usage: python3 read-data.py [file name]")
+        sys.exit()
+        
+    data = readData(sys.argv[1])
